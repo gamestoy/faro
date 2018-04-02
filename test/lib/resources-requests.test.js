bb74d7c4532845e7f4fee284162d92b6090e1487
gamestoy <gamestoy@despegar.com>
2018-04-02 01:54:08 -0300

    Add group by url

--- test/lib/resources-requests.test.js
+++ test/lib/resources-requests.test.js
@@ -8,12 +8,64 @@ test('timeline', async () => {
   const traces = await getTraces('../resources/traces.json');
   const resources = ResourcesRequests.parse(new DevtoolsTimelineModel(traces), Number.MAX_VALUE);
   const timeline = S.fromMaybe([], resources);
-  expect(timeline.length).toBe(132);
+  expect(timeline).toHaveLength(129);
   const firstTrace = timeline[0];
-  expect(firstTrace.url).toBe('https://www.despegar.com.ar/search/Hotel/982/2018-04-02/2018-04-04/2');
-  expect(firstTrace.transferSize).toBe(99124);
-  expect(firstTrace.decodedBodyLength).toBe(539334);
-  expect(firstTrace.mimeType).toBe('text/html');
-  expect(firstTrace.priority).toBe('VeryHigh');
-  expect(firstTrace.duration).toBe(1622);
+  const cssTrace = timeline[1];
+
+  expect(firstTrace).toEqual({
+    id: 'DF6152C9C008B7CAAA026A5493F68E07',
+    url: 'https://www.despegar.com.ar/search/Hotel/982/2018-04-02/2018-04-04/2',
+    startTime: 0,
+    duration: 1622,
+    priority: 'VeryHigh',
+    transferSize: 99124,
+    decodedBodyLength: 539334,
+    type: 'html',
+    status: 200,
+    timing: {
+      connectStart: 72,
+      connectEnd: 534,
+      dnsStart: 14,
+      dnsEnd: 72,
+      proxyStart: -1,
+      proxyEnd: -1,
+      pushEnd: 0,
+      pushStart: 0,
+      requestStart: 534,
+      requestEnd: 535,
+      sslEnd: 534,
+      sslStart: 220,
+      responseStart: 1103,
+      responseEnd: 1622,
+    },
+  });
+
+  expect(cssTrace).toEqual({
+    id: '12391.2',
+    url: 'https://ar.staticontent.com/hotels/search/public/css/pkg/eva-core.min.ddd45316.css',
+    startTime: 1172,
+    duration: 256,
+    priority: 'VeryHigh',
+    transferSize: 3692,
+    decodedBodyLength: 13143,
+    type: 'stylesheet',
+    status: 200,
+    initiator: 'https://www.despegar.com.ar/search/Hotel/982/2018-04-02/2018-04-04/2',
+    timing: {
+      connectStart: 18,
+      connectEnd: 77,
+      dnsStart: 0,
+      dnsEnd: 18,
+      proxyStart: -1,
+      proxyEnd: -1,
+      pushEnd: 0,
+      pushStart: 0,
+      requestStart: 82,
+      requestEnd: 83,
+      sslEnd: 77,
+      sslStart: 31,
+      responseStart: 100,
+      responseEnd: 256,
+    },
+  });
 });
