42d523da2eea88fd3a393afec60860f513ea46b8
gamestoy <gamestoy@despegar.com>
2018-04-04 03:25:02 -0300

    Fix request builder
    The request values were informed incorrectly when a redirect happened

--- test/lib/summary.test.js
+++ test/lib/summary.test.js
@@ -4,10 +4,10 @@ const { buildSummary } = require('../../lib/summary');
 test('create summary', async () => {
   const traces = await getTraces('../resources/resources.json');
   const grouped = buildSummary(traces);
-  expect(grouped.all.transferSize).toBe(1336525);
-  expect(grouped.all.decodedBodyLength).toBe(3973711);
-  expect(grouped.javascript.transferSize).toBe(477640);
-  expect(grouped.javascript.decodedBodyLength).toBe(1569540);
+  expect(grouped.all.transferSize).toBe(2437774);
+  expect(grouped.all.decodedBodyLength).toBe(6629843);
+  expect(grouped.javascript.transferSize).toBe(860270);
+  expect(grouped.javascript.decodedBodyLength).toBe(3310242);
   expect(grouped.stylesheet.transferSize).toBe(471446);
   expect(grouped.stylesheet.decodedBodyLength).toBe(1370408);
 });
